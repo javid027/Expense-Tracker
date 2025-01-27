0cae5ea102744c43f2d4e489029364694bde6232
Javid Ansari <94390464+javid027@users.noreply.github.com>
2025-01-27 23:31:54 +0530

    Updated

--- expensetracker/lib/main.dart
+++ expensetracker/lib/main.dart
@@ -1,125 +1,230 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:path/path.dart';
+import 'package:provider/provider.dart';
+import 'package:sqflite/sqflite.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ExpenseProvider(),
+    child: ExpenseTrackerApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class ExpenseTrackerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Expense Tracker',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.teal,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ExpenseHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class ExpenseProvider extends ChangeNotifier {
+  List<Map<String, dynamic>> _expenses = [];
+  late Database _database;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  List<Map<String, dynamic>> get expenses => _expenses;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  ExpenseProvider() {
+    _initializeDatabase();
+  }
 
-  final String title;
+  Future<void> _initializeDatabase() async {
+    final dbPath = await getDatabasesPath();
+    _database = await openDatabase(
+      join(dbPath, 'expenses.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE expenses(id INTEGER PRIMARY KEY, amount REAL, description TEXT, date TEXT)',
+        );
+      },
+      version: 1,
+    );
+    _fetchExpenses();
+  }
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Future<void> _fetchExpenses() async {
+    final data = await _database.query('expenses');
+    _expenses = data;
+    notifyListeners();
+  }
+
+  Future<void> addExpense(double amount, String description) async {
+    final newExpense = {
+      'amount': amount,
+      'description': description,
+      'date': DateTime.now().toIso8601String(),
+    };
+    await _database.insert('expenses', newExpense);
+    _fetchExpenses();
+  }
+
+  Future<void> deleteExpense(int id) async {
+    await _database.delete('expenses', where: 'id = ?', whereArgs: [id]);
+    _fetchExpenses();
+  }
+
+  Future<void> updateExpense(int id, double amount, String description) async {
+    final updatedExpense = {
+      'amount': amount,
+      'description': description,
+      'date': DateTime.now().toIso8601String(),
+    };
+    await _database
+        .update('expenses', updatedExpense, where: 'id = ?', whereArgs: [id]);
+    _fetchExpenses();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ExpenseHomePage extends StatefulWidget {
+  @override
+  _ExpenseHomePageState createState() => _ExpenseHomePageState();
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _ExpenseHomePageState extends State<ExpenseHomePage> {
+  final _amountController = TextEditingController();
+  final _descriptionController = TextEditingController();
+  int? _editingId;
+
+  void _showAddExpenseDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('Add New Expense'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _amountController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'Amount'),
+            ),
+            TextField(
+              controller: _descriptionController,
+              decoration: InputDecoration(labelText: 'Description'),
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(ctx).pop(),
+            child: Text('Cancel'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              final amount = double.tryParse(_amountController.text);
+              final description = _descriptionController.text;
+
+              if (amount != null && description.isNotEmpty) {
+                if (_editingId == null) {
+                  Provider.of<ExpenseProvider>(context, listen: false)
+                      .addExpense(amount, description);
+                } else {
+                  Provider.of<ExpenseProvider>(context, listen: false)
+                      .updateExpense(_editingId!, amount, description);
+                }
+                Navigator.of(ctx).pop();
+              }
+            },
+            child: Text('Save'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showEditExpenseDialog(
+      BuildContext context, Map<String, dynamic> expense) {
+    _amountController.text = expense['amount'].toString();
+    _descriptionController.text = expense['description'];
+    _editingId = expense['id'];
+
+    debugPrint("amt:${expense['amount'].toString()}");
+    debugPrint("description:${expense['description']}");
+    debugPrint("id:${expense['id']}");
+    _showAddExpenseDialog(context);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final expenseProvider = Provider.of<ExpenseProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Expense Tracker'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                _amountController.clear();
+                _descriptionController.clear();
+                _editingId = null;
+                _showAddExpenseDialog(context);
+              }),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: expenseProvider.expenses.isEmpty
+          ? Center(
+              child: Text(
+                'No expenses added yet!',
+                style: TextStyle(fontSize: 18),
+              ),
+            )
+          : ListView.builder(
+              itemCount: expenseProvider.expenses.length,
+              itemBuilder: (ctx, index) {
+                final expense = expenseProvider.expenses[index];
+                return Card(
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: FittedBox(
+                          child:
+                              Text('\$${expense['amount'].toStringAsFixed(2)}'),
+                        ),
+                      ),
+                    ),
+                    title: Text(expense['description']),
+                    subtitle: Text(DateFormat.yMMMd()
+                        .format(DateTime.parse(expense['date']))),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.edit),
+                          onPressed: () =>
+                              _showEditExpenseDialog(context, expense),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () {
+                            expenseProvider.deleteExpense(expense['id']);
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          child: Icon(Icons.add),
+          onPressed: () {
+            _amountController.clear();
+            _descriptionController.clear();
+            _editingId = null;
+            _showAddExpenseDialog(context);
+          }),
     );
   }
 }
